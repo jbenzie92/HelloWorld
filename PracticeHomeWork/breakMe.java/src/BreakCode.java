35a28a263d613439f5a2b8bffb00a89b0a7a5e88
John Williams <johnwilliams@johnwiliamssmbp.home>
2020-11-11 20:38:38 -0500

    first commit

---
+++ PracticeHomeWork/breakMe.java/src/BreakCode.java
@@ -0,0 +1,23 @@
+public class BreakCode {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+    Integer badInteger = 2.87652;
+    System.out.println(badInteger);
+    Float badFloat = 20;
+    System.out.println(badFloat);
+    Byte badByte = 50000000;
+    System.out.println(badByte);
+    String badString = 238796;
+    System.out.println(badString);
+    byte b = (byte) 1293;
+    System.out.println(b);
+    String appName = "Terrible App";
+    System.out.println(appName);
+
+    // I don't understand this assignment at all! 
+
+
+
+    }
+}
